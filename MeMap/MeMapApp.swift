92721bb4e0a8a38209d8c64cea60e0fc5b7b7f88
“kurniawanj26” <jayadi.kurniawan@gmail.com>
2023-08-25 14:15:11 +0700

    Initial Commit

---
+++ MeMap/MeMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeMapApp.swift
+//  MeMap
+//
+//  Created by Jayadi Kurniawan on 25/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MeMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
